d5b2b4b3a71bbaf1d7d07c1cafa926c20e1530a7
anon123123123 <cditodavis96@gmail.com>
2022-01-14 22:45:45 -0700

    Got scoring working and added to html

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,29 @@
+const scoresSection = document.querySelector('#high-scores')
+const olElem = document.createElement("ol")
+
 const data = JSON.parse(localStorage.scores)
 const sortedData = data.sort((a, b) => b[1] - a[1])
-console.log(sortedData)
+
+const addScores = async()=> {
+    // let scores = await getScores()
+    scoresSection.append(olElem)
+    sortedData.forEach(element => {
+        let userScore = `User: ${element[0]} Score: ${element[1]}`
+        let liElem = document.createElement("li")
+        olElem.append(liElem)
+        console.log(element)
+        liElem.appendChild(document.createTextNode(userScore));
+        
+    });
+
+
+}
+
+// const getScores = async()=> {
+//     const data = JSON.parse(localStorage.scores)
+//     const sortedData = data.sort((a, b) => b[1] - a[1])
+//     return sortedData
+// }
+
+
+addScores()
